cc97fea76d00bfb4e5525a49c55ccc3261dde93c
hcp-andrew <andrew@holycowprod.com>
2022-08-28 20:02:28 +0530

    Final scenes

---
+++ Assets/Scritps/Interactions/FinalTrigger.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class FinalTrigger : MonoBehaviour
+{
+    [SerializeField] float FinalSize;
+    [SerializeField] Vector3 FinalPosition;
+    [SerializeField] float MoveSpeed;
+    bool ShouldMove;
+
+    private void Update()
+    {
+        if (ShouldMove)
+        {
+            Camera.main.orthographicSize = Mathf.Lerp(Camera.main.orthographicSize, FinalSize, MoveSpeed * Time.deltaTime);
+            Camera.main.transform.position = Vector3.Lerp(Camera.main.transform.position, FinalPosition, MoveSpeed * Time.deltaTime);
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag(Constants.PlayerTag))
+        {
+            collision.GetComponent<PlayerController>().enabled = false;
+            ShouldMove = true;
+        }
+    }
+}
