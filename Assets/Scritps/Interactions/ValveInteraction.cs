23440cc2d4422721c51d6be0e42c6579f03ffdae
hcp-andrew <andrew@holycowprod.com>
2022-08-28 14:29:16 +0530

    Added sounds Created prefab for player

--- Assets/Scritps/Interactions/ValveInteraction.cs
+++ Assets/Scritps/Interactions/ValveInteraction.cs
@@ -5,30 +5,31 @@ using UnityEngine.Events;
 
 public class ValveInteraction : InteractionBase
 {
-    bool IsReady = false;
+    [SerializeField] bool IsReady = false;
     [SerializeField] Transform Crank;
     [SerializeField] float SpinSpeed;
     [SerializeField] float SpinDuration;
     [SerializeField] UnityEvent FinalAction;
+    [SerializeField] AudioClip CorrectSound;
+    [SerializeField] AudioClip WrongSound;
     public override void PerformInteraction()
     {
+
         if (IsReady)
         {
-            //TODO: Play Crank sound
             StartCoroutine(SpinAction());
-            Debug.Log("Wrong");
+            GetComponent<AudioSource>().PlayOneShot(CorrectSound);
         }
         else
         {
-            //TODO: Play wrong sound
-            Debug.Log("Wrong");
+            GetComponent<AudioSource>().PlayOneShot(WrongSound);
         }
     }
 
     IEnumerator SpinAction()
     {
         float f = 0;
-        while(f < SpinDuration)
+        while (f < SpinDuration)
         {
             f += Time.deltaTime;
             Crank.Rotate(0, 0, SpinSpeed * Time.deltaTime);
